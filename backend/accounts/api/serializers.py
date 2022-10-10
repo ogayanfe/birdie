3b7769a04fa8ad5ead59a71663c3a801270f2be5
Odule Ayanfeoluwa <ayanfeoluwakoyinsola@gmail.com>
2022-10-10 22:09:59 +0100

    fix/password-should-be-hashed-in-database

--- backend/accounts/api/serializers.py
+++ backend/accounts/api/serializers.py
@@ -52,3 +52,20 @@ class SignupSerializer(serializers.ModelSerializer):
                 'write_only': True,
             }
         }
+
+    def create(self, validated_data):
+        password = validated_data.pop('password', None)
+        instance = self.Meta.model(**validated_data)
+        if password is not None:
+            instance.set_password(password)
+        instance.save()
+        return instance
+
+    def update(self, instance, validated_data):
+        for attr, value in validated_data.items():
+            if attr == 'password':
+                instance.set_password(value)
+            else:
+                setattr(instance, attr, value)
+        instance.save()
+        return instance
