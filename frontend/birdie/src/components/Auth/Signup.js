394e277960454e25abf543c0c7648b7b33436660
Odule Ayanfeoluwa <ayanfeoluwakoyinsola@gmail.com>
2022-11-19 20:43:11 +0100

    fix: add validation to signup form

--- frontend/birdie/src/components/Auth/Signup.js
+++ frontend/birdie/src/components/Auth/Signup.js
@@ -2,6 +2,13 @@ import React, { useState } from "react";
 import useUserContext from "../../contexts/UserContext";
 import { Navigate, Link } from "react-router-dom";
 
+const validUsernamePattern = /^[\w.@+-]+$/;
+
+function validateUsername(username) {
+    if (!username || username.at(-1) === " ") return false;
+    return Boolean(username.match(validUsernamePattern));
+}
+
 export default function SignUp() {
     const { user, signup } = useUserContext();
     const [formData, setFormData] = useState({
@@ -9,10 +16,12 @@ export default function SignUp() {
         password: "",
         passwordConfirm: "",
     });
-
+    const [usernameError, setUsernameError] = useState(false);
+    const [passwordError, setPasswordError] = useState(false);
     function handleSubmit(e) {
         e.preventDefault();
-        signup(formData);
+        if (formData.password !== formData.passwordConfirm) setPasswordError(true);
+        else signup(formData);
     }
 
     const handleChange = (e) => {
@@ -20,12 +29,15 @@ export default function SignUp() {
             ...prev,
             [e.target.name]: e.target.value,
         }));
+        if (!validateUsername(e.target.value)) {
+            setUsernameError(true);
+        } else setUsernameError(false);
     };
 
     if (user) return <Navigate to="/" />;
     return (
         <main className="w-screen h-screen flex items-center justify-center">
-            <div className="w-[90vw] bg-purple-50 max-w-lg p-6 h-max-content rounded-lgz border-l-4">
+            <div className="w-[93vw] bg-purple-50 max-w-lg p-6 h-max-content rounded-lgz border-l-4">
                 <h1 className="text-2xl text-purple-500 flex justify-center gap-1 items-center m-2">
                     <span className="text-3xl">
                         <iconify-icon icon="game-icons:hummingbird"></iconify-icon>
@@ -34,6 +46,12 @@ export default function SignUp() {
                 </h1>
                 <form onSubmit={(e) => handleSubmit(e)} className="w-full flex flex-col gap-3">
                     <label htmlFor="signup-username">Username</label>
+
+                    {usernameError && (
+                        <p className="text-sm text-red-500">
+                            Invalid username, only letters, numbers, and @/./+/-/_ characters.
+                        </p>
+                    )}
                     <input
                         type="text"
                         autoFocus
@@ -41,9 +59,11 @@ export default function SignUp() {
                         required
                         className="rounded-lg p-2 text-sm"
                         onChange={handleChange}
+                        value={formData.username}
                         placeholder="username"
                         id="signup-username"
                     />
+                    {passwordError && <p className="text-sm text-red-500">Passwords must match</p>}
                     <label htmlFor="signup-password">Password</label>
                     <input
                         type="password"
@@ -51,16 +71,19 @@ export default function SignUp() {
                         name="password"
                         id="signup-password"
                         required
+                        value={formData.password}
                         onChange={handleChange}
                         placeholder="password"
                     />
 
                     <label htmlFor="signup-password-confirm">Password confirmation</label>
+
                     <input
                         type="password"
                         className="rounded-lg p-2 text-sm"
                         name="passwordConfirm"
                         id="signup-password-confirm"
+                        value={formData.passwordConfirm}
                         required
                         onChange={handleChange}
                         placeholder="password confirm"
